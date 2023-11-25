d8c62f1c5755cdf176ba52ed83caaa03d3e34a12
TeymurDevv <transmissiyaservis@gmail.com>
2023-11-25 14:50:51 +0400

    Static Example Created Increasing ID and Password Checker mechanism is created

---
+++ Changing/Models/User.cs
@@ -0,0 +1,49 @@
+﻿using Changing.interFaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Changing.Models
+{
+    internal class User:IAccount
+
+    {
+       
+        public int Id;
+        public string Fullname;
+        public string Email;
+        public string Password;
+        public static int IdCount = 1;
+
+        public User(string fullname, string email, string password)
+        {
+            Id = IdCount++;
+            Fullname = fullname;
+            Email = email;
+            Password = password;
+        }
+        public void ShowingInfo()
+        {
+            Console.WriteLine($"{Fullname} {Password} {Id}");
+        }
+        public  bool checkPssword(string password)
+        {
+            if (password.Length > 8 &&
+               password.Any(char.IsUpper) &&
+               password.Any(char.IsLower) &&
+               password.Any(char.IsDigit))
+            {
+               
+                return true;
+            }
+            else
+            {
+               
+                return false;
+            }
+        }
+    }
+}
+
---
+++ Changing/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Changing.Models;
+
+Console.WriteLine("Hello, World!");
+User user = new("Orxan ibrahimzade", "Orxaan12@gmail.com", "Teymur123");
+user.checkPssword(user.Password);
+user.ShowingInfo();
